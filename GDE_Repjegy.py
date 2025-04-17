411c22004da8e59d50283034b00bd4c3d8e29f1d
API65I <amolnart@gmail.com>
2025-04-17 22:51:11 +0200

    Járat osztályok: Jarat és alosztályok Belfoldi, Nemzetkozi típusok

---
+++ GDE_Repjegy.py
@@ -0,0 +1,26 @@
+
+class Jarat:
+    def __init__(self, jaratszam, celallomas, jegyar):
+        self.jaratszam = jaratszam
+        self.celallomas = celallomas
+        self.jegyar = jegyar
+
+class Belfoldi(Jarat):
+    def __init__(self, jaratszam, celallomas):
+        super().__init__(jaratszam, celallomas, 15000)
+
+class Nemzetkozi(Jarat):
+    def __init__(self, jaratszam, celallomas, alap_ar):
+        super().__init__(jaratszam, celallomas, alap_ar)
+
+class Europa(Nemzetkozi):
+    def __init__(self, jaratszam, celallomas):
+        super().__init__(jaratszam, celallomas, 45000)
+
+class Amerika(Nemzetkozi):
+    def __init__(self, jaratszam, celallomas):
+        super().__init__(jaratszam, celallomas, 85000)
+
+class Azsia(Nemzetkozi):
+    def __init__(self, jaratszam, celallomas):
+        super().__init__(jaratszam, celallomas, 95000)
